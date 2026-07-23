234c163cd1e344d95efcdf2b5f7ad02f21a15f65
unknown <danielhs8@upi.edu>
2026-07-23 16:44:50 +0700

    feat(sales): prune unneeded fields and metadata from sales api response and swagger docs

--- src/modules/sales/entities/sale.entity.ts
+++ src/modules/sales/entities/sale.entity.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { Transaction, TransactionItem, Payment } from '@prisma/client';
-import { ProductEntity } from '@modules/products/entities/product.entity';
+import { Transaction, TransactionItem, Payment, Product } from '@prisma/client';
+import { Exclude } from 'class-transformer';
 
 export class PaymentEntity implements Partial<Payment> {
   @ApiProperty()
@@ -24,10 +24,10 @@ export class PaymentEntity implements Partial<Payment> {
   @ApiPropertyOptional()
   paid_at: Date | null;
 
-  @ApiProperty()
+  @Exclude()
   created_at: Date;
 
-  @ApiProperty()
+  @Exclude()
   updated_at: Date;
 
   constructor(partial: Partial<PaymentEntity>) {
@@ -35,8 +35,44 @@ export class PaymentEntity implements Partial<Payment> {
   }
 }
 
-export class SaleItemEntity implements Partial<TransactionItem> {
+export class SaleItemProductEntity implements Partial<Product> {
   @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  sku: string;
+
+  @ApiProperty()
+  price: number;
+
+  @Exclude()
+  id: number;
+
+  @Exclude()
+  description: string | null;
+
+  @Exclude()
+  is_active: boolean;
+
+  @Exclude()
+  category_id: number;
+
+  @Exclude()
+  created_at: Date;
+
+  @Exclude()
+  updated_at: Date;
+
+  @Exclude()
+  deleted_at: Date | null;
+
+  constructor(partial: Partial<SaleItemProductEntity>) {
+    Object.assign(this, partial);
+  }
+}
+
+export class SaleItemEntity implements Partial<TransactionItem> {
+  @Exclude()
   id: number;
 
   @ApiProperty()
@@ -48,16 +84,19 @@ export class SaleItemEntity implements Partial<TransactionItem> {
   @ApiProperty()
   subtotal: number;
 
-  @ApiProperty()
+  @Exclude()
   product_id: number;
 
-  @ApiPropertyOptional({ type: () => ProductEntity })
-  product?: ProductEntity;
+  @Exclude()
+  transaction_id: number;
+
+  @ApiPropertyOptional({ type: () => SaleItemProductEntity })
+  product?: SaleItemProductEntity;
 
   constructor(partial: Partial<SaleItemEntity>) {
     Object.assign(this, partial);
     if (partial.product) {
-      this.product = new ProductEntity(partial.product);
+      this.product = new SaleItemProductEntity(partial.product);
     }
   }
 }
@@ -84,16 +123,16 @@ export class SaleEntity implements Partial<Transaction> {
   @ApiProperty()
   status: string;
 
-  @ApiProperty()
+  @Exclude()
   user_id: number;
 
-  @ApiProperty()
+  @Exclude()
   created_at: Date;
 
-  @ApiProperty()
+  @Exclude()
   updated_at: Date;
 
-  @ApiPropertyOptional()
+  @Exclude()
   deleted_at: Date | null;
 
   @ApiPropertyOptional({ type: [SaleItemEntity] })
